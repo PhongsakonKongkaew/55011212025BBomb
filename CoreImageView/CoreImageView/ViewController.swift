4bdad1be06fd007e669c94024a590d4b04169e11
PhongsakonKongkaew <istudents@iClassrooms-iMac-5.local>
2015-02-20 17:21:44 +0700

    Week 6
    Week 20-02-2015

--- CoreImageView/CoreImageView/ViewController.swift
+++ CoreImageView/CoreImageView/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AssetsLibrary
 
 class ViewController: UIViewController {
 
@@ -40,7 +41,9 @@ class ViewController: UIViewController {
         
         // 2
         let newImage = UIImage(CGImage: cgimg!)
-        self.UIImageCiew.image = newImage
+        self.imageView.image = newImage
+        
+        self.logAllfilters()
         
         
     }
@@ -48,6 +51,39 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func logAllfilters(){
+        let properties = CIFilter.filterNamesInCategory(kCICategoryBuiltIn)
+        println(properties)
+        
+        for filterName: AnyObject in properties{
+            let fltr = CIFilter(name: filterName as String)
+            println(fltr.attributes())
+        }
+    }
+    
+    func oldPhoto(img:CIImage, withAmount intensity:Float) -> CIImage{
+        // 1
+        let sepia = CIFilter(name: "CISepiaTone")
+        sepia.setValue(img, forKey: kCIInputImageKey)
+        sepia.setValue(intensity, forKey: "inputIntensity")
+        
+        // 2
+        let random = CIFilter(name: "CIRandomGenerator")
+        
+        // 3
+        let lighten = CIFilter(name: "CIColorControls")
+        lighten.setValue(random.outputImage, forKey:  kCIInputImageKey)
+        lighten.setValue(1-intensity, forKey: "inputBrightness")
+        lighten.setValue(0, forKey: "inputSaturation")
+        
+        
+        // 4 
+        let croppedImage = lighten.outputImage.imageByCroppingToRect(beginImage.extent())
+        
+        // 5 
+        let composite = CIFilter(name: "CIHardLightBlendMode")
     }
 
 
